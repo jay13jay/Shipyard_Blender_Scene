7e98c8cc683d7462dc17caf12869e85cb5c92b6e
Josh <josh.hackney13@gmail.com>
2021-04-07 19:43:10 -0400

    renamed a file and changed some minor functional things in the python readme gen script

--- Renders/genreadme.py
+++ Renders/genreadme.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python3
 import re
+
 from pathlib import Path
 
-txt_folder = Path('./').rglob('*.png')
-files = [x for x in txt_folder]
 
-count = 1
-for f in files:
-  new_f = re.sub(".png|[0-9]|\.", "", str(f))
-  new_f = re.sub("-|_", " ", new_f)
-  # new_f = re.sub("")
-  print("File {}:\t old name: {} \t\t new name: {}".format(count, f, new_f))
-  count += 1
+def debug_msg(msg):
+  dmsg.append(msg)
+
+def get_names(files): 
+  count = 1
+  for f in files:
+    new_f = re.sub(".png|\.", "", str(f))
+    new_f = re.sub("-|_", " ", new_f)
+    msg = "File {}: File name: {} Title: {}".format(count, f, new_f)
+    names.append(new_f)
+
+    debug_msg(msg)
+    count += 1
+  return (names)
+
+def main():
+  # Set globals
+  global txt_folder
+  global files
+  global dmsg
+  global names
+  global debug
+
+  txt_folder = Path('./').rglob('*.png')
+  files = [x for x in txt_folder]
+  dmsg = []
+  names = []
+  debug = 0
+
+  # Start main logic
+ 
+  names = get_names(files)
+  debug_msg("Names list: {}".format(names))
+
+  if debug == 1:
+    for m in dmsg:
+      print("DEBUG: {}".format(m))
+
+
+if __name__ == "__main__":
+  main()
